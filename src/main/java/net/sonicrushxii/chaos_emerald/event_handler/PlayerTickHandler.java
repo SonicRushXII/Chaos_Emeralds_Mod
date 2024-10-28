d1282d074031da8481ed317f24397e2543160d34
My Laptop <jonat146@gmail.com>
2024-10-28 20:29:33 +0530

    Tried to Add IceSpike

--- src/main/java/net/sonicrushxii/chaos_emerald/event_handler/PlayerTickHandler.java
+++ src/main/java/net/sonicrushxii/chaos_emerald/event_handler/PlayerTickHandler.java
@@ -1,16 +1,9 @@
 package net.sonicrushxii.chaos_emerald.event_handler;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.sonicrushxii.chaos_emerald.modded.ModBlocks;
-
-import java.util.Set;
 
 public class PlayerTickHandler {
     @SubscribeEvent
